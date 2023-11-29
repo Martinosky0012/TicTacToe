ca3e99721d9059fd9f0a4a09082c67ca75460189
Martinosky0012 <martisimonrojas@gmail.com>
2023-11-29 18:44:22 +0100

    start TicTacToeController PlayGame add:isValidMove()

--- src/TicTacToeController/TicTacToeController.java
+++ src/TicTacToeController/TicTacToeController.java
@@ -2,6 +2,8 @@ package TicTacToeController;
 import TicTacToeModel.TicTacToeModel;
 import TicTacToeView.TicTacToeView;
 
+import java.util.Scanner;
+
 public class TicTacToeController {
     private TicTacToeModel model;
     private TicTacToeView view;
@@ -11,13 +13,19 @@ public class TicTacToeController {
         this.view = view;
     }
     public void playGame() {
+        Scanner scanner = new Scanner(System.in);
+        //Game loop
+        do {
+            view.displayBoard(model.getBoard());
+            int row, col;
 
+        } while (isGameFinished()==-1);
     }
     private boolean isValidMove(int row, int col) {
-
-        return false;
+        return (row >= 0 && row < 3 && col >= 0 && col < 3 && model.getBoard()[row][col] == ' ');
     }
-    private int isGameFinished() {
+    private int isGameFinished() {// 1: win, 0: draw, -1:not finished
+
         return 0;
     }
 }
